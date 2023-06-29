a04082b30a398861739e977113607fb09e1ef125
Chanbroset Prach <chanbroset.prach@alpegagroup.com>
2023-06-29 08:07:14 +0700

    add screen condition

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,15 @@ module.exports = {
     "./src/**/*.{ts,tsx}",
   ],
   theme: {
+    screens: {
+      xxs: "200px",
+      xs: "350px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
     container: {
       center: true,
       padding: "2rem",
